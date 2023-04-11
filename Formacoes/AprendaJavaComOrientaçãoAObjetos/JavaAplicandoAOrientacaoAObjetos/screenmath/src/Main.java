75c060aaa1c74ea21a986be0c8232cccf723d207
duduzx02 <duduzx02@gmail.com>
2023-04-11 02:09:08 -0300

    Atribuindo valores ao nosso filme

---
+++ Formacoes/AprendaJavaComOrientaçãoAObjetos/JavaAplicandoAOrientacaoAObjetos/screenmath/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie myMovie = new Movie();
+        myMovie.name = "O poderoso chefão";
+        myMovie.releaseyear = 1970;
+        myMovie.durationInMinutes = 180;
+
+        System.out.println(myMovie.name);
+
+    }
+}
