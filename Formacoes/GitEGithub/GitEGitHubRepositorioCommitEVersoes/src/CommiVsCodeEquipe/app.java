7f8e946b7ffd9adefcc1644ce8be0b629f6b8a1d
duduzx02 <duduzx02@gmail.com>
2023-04-21 02:31:57 -0300

    Criando o arquivo Java com o System.out.println()

---
+++ Formacoes/GitEGithub/GitEGitHubRepositorioCommitEVersoes/src/CommiVsCodeEquipe/app.java
@@ -0,0 +1,7 @@
+package CommiVsCodeEquipe;
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Rodandno o sistema de gerenciamento de jogos!");
+    }
+}
