ea563093a8f0ea9542052059a523c2072e10951b
duduzx02 <duduzx02@gmail.com>
2023-04-25 01:15:58 -0300

    Relacionamentos com coleções

---
+++ Formacoes/JavaCollections/Gerenciador-de-cursos/src/br/com/alura/TestaCurso.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando Coleções do Java", "Paulo");
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+        javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+
+        javaColecoes.getAulas().forEach(aula -> {
+            System.out.println(aula);
+        });
+/*        System.out.println(javaColecoes.getAulas());*/
+
+
+    }
+}
