dea986bdfce0d02086bbdb4b1ade3b7660333ca7
duduzx02 <duduzx02@gmail.com>
2023-04-24 23:07:40 -0300

    Adicionando elementos em um ArrayList

---
+++ Formacoes/JavaCollections/Gerenciador-de-cursos/src/br/com/alura/TestandoListas2.java
@@ -0,0 +1,22 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestandoListas2 {
+    public static void main(String[] args) {
+        String curso1 = "Java 8";
+        String curso2 = "Apache Camel";
+        String curso3 = "Certificação Java SE 8";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso1);
+        cursos.add(curso2);
+        cursos.add(curso3);
+
+        Collections.sort(cursos);
+        cursos.forEach(curso -> {
+            System.out.println("Curso : " + curso);
+        });
+    }
+}
