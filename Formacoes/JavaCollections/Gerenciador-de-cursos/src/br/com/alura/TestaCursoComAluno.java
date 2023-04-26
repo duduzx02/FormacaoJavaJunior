9ae29100b825a919805d567743128c9b3e9b3bfc
duduzx02 <duduzx02@gmail.com>
2023-04-26 11:07:22 -0300

    Colocando mais um aluno

--- Formacoes/JavaCollections/Gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
+++ Formacoes/JavaCollections/Gerenciador-de-cursos/src/br/com/alura/TestaCursoComAluno.java
@@ -1,5 +1,7 @@
 package br.com.alura;
 
+import java.util.Set;
+
 public class TestaCursoComAluno {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando Coleções do Java", "Paulo");
@@ -11,15 +13,17 @@ public class TestaCursoComAluno {
         Aluno a1 = new Aluno("Rodrigo Turini", 34672);
         Aluno a2 = new Aluno("Guilherme Silveira", 5617);
         Aluno a3 = new Aluno("Mauricio Aniche", 17645);
+        Aluno a4 = new Aluno("Eduardo Lima", 15879);
 
         javaColecoes.matricula(a1);
         javaColecoes.matricula(a2);
         javaColecoes.matricula(a3);
-
+        javaColecoes.matricula(a4);
 
         System.out.println("Todos os alunos matriculados: ");
         javaColecoes.getAlunos().forEach(aluno -> {
             System.out.println(aluno);
         });
+
     }
 }
