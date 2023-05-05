4b02a6fbeae4c305a3cf4270e5a2e057371ea677
duduzx02 <duduzx02@gmail.com>
2023-05-05 00:56:09 -0300

    Aplicando o pattern Composite

--- Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/DesignPatternsEmJavaIBoasPraticasDeProgramacao/loja/src/br/com/alura/loja/TesteDescontos.java
+++ Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/DesignPatternsEmJavaIBoasPraticasDeProgramacao/loja/src/br/com/alura/loja/TesteDescontos.java
@@ -4,6 +4,7 @@ import br.com.alura.loja.descontos.CalculadoraDeDecontos;
 import br.com.alura.loja.impostos.CalculadoraDeImpostos;
 import br.com.alura.loja.impostos.ICMS;
 import br.com.alura.loja.impostos.ISS;
+import br.com.alura.loja.orcamento.ItemOrcamento;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -11,11 +12,17 @@ import java.math.BigDecimal;
 public class TesteDescontos {
     public static void main(String[] args) {
         Orcamento primeiro = new Orcamento();
+        primeiro.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
 
         Orcamento segundo = new Orcamento();
+        segundo.adicionarItem(new ItemOrcamento(new BigDecimal("1000")));
+
+        Orcamento terceiro = new Orcamento();
+        terceiro.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
 
         CalculadoraDeDecontos calculadoraDeDecontos = new CalculadoraDeDecontos();
         System.out.println(calculadoraDeDecontos.calcular(primeiro));
         System.out.println(calculadoraDeDecontos.calcular(segundo));
+        System.out.println(calculadoraDeDecontos.calcular(terceiro));
     }
 }
--- Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/DesignPatternsEmJavaIBoasPraticasDeProgramacao/loja/src/br/com/alura/loja/pedido/GeraPeidoHandler.java
+++ Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/DesignPatternsEmJavaIBoasPraticasDeProgramacao/loja/src/br/com/alura/loja/pedido/GeraPeidoHandler.java
@@ -15,7 +15,7 @@ public class GeraPeidoHandler {
     }
 
     public void execute(GeraPedido dados){
-        Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
+        Orcamento orcamento = new Orcamento();
 
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 
