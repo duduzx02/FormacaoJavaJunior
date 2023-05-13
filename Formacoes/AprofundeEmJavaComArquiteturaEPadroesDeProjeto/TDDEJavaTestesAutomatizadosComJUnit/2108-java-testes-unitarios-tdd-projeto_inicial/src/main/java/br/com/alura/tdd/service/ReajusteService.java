64255a13dda22c88d013611d91cbfbaf9bfdbad8
duduzx02 <duduzx02@gmail.com>
2023-05-13 04:17:49 -0300

    Refactoring

--- Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/TDDEJavaTestesAutomatizadosComJUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ Formacoes/AprofundeEmJavaComArquiteturaEPadroesDeProjeto/TDDEJavaTestesAutomatizadosComJUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,15 +7,10 @@ import java.math.BigDecimal;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+
+            BigDecimal percentualReajuste = desempenho.percentualReajuste();
+            BigDecimal reajuste = funcionario.getSalario().multiply(percentualReajuste);
             funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.1"));
-            funcionario.reajustarSalario(reajuste);
-        } else if (desempenho == Desempenho.OTIMO) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
-            funcionario.reajustarSalario(reajuste);
-        }
+
     }
 }
